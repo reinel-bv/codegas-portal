80fc21a6ff6ff0d8d7690f8eac1049d40ee974df
Fernando Ortiz <sirortiz@192.168.0.7>
2023-07-12 02:23:45 -0500

    change url

--- src/app/utils/url.ts
+++ src/app/utils/url.ts
@@ -1,3 +1,3 @@
-const URL = 'https://uk1e5xqrv4.execute-api.us-east-1.amazonaws.com'
+const URL = 'https://216vhep1ye.execute-api.us-east-1.amazonaws.com'
 // const URL = 'http://localhost:4000'
 export default URL;
