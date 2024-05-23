3049b81f2d7cfb4dc681500be8692b3dc8d3641b
fernandooj <fernandooj@ymail.com>
2024-05-23 14:52:48 -0500

    add new data

--- src/app/zonas/table.tsx
+++ src/app/zonas/table.tsx
@@ -1,27 +1,56 @@
 'use client' 
-import { useEffect, useState } from 'react';
+import { ChangeEvent, KeyboardEvent, useEffect, useState } from 'react';
 import { TableRow, TableCell, Checkbox, FormControl, InputLabel, InputAdornment, OutlinedInput, Paper, Table, TableBody, TableContainer, TableHead } from '@mui/material';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 import {Snack} from "../components/snackBar"
-import {ChangeValorUnitario, ChangeValorUnitarioAll} from "../store/fetch-zona"
+import {ChangeValorUnitario, ChangeValorUnitarioAll, ChangeValorUnitarioSelected} from "../store/fetch-zona"
 import { PaginationTable } from "../components/pagination/pagination";
 import InputZones from '../components/input_zones/input_zones';
 
-
 const RenderZonas = ({zona, updateValor, addValues}: any) => {
+  const [valores, setValores] = useState<{ [key: string]: string }>({});
+
+  useEffect(() => {
+    // Inicializar el estado con los valores de valorunitario
+    const initialValues: { [key: string]: string } = {};
+    zona.forEach(({ _id, valorunitario }: any) => {
+      initialValues[_id] = valorunitario;
+    });
+    setValores(initialValues);
+  }, [zona]);
+
+  const handleInputChange = (e: any, _id: any) => {
+    const { value } = e.target;
+    // Actualizar el estado con el nuevo valor de valorunitario
+    setValores(prevState => ({
+      ...prevState,
+      [_id]: value
+    }));
+  };
+
+  const handleKeyDown = (e: any, idcliente: any, nombre: any) => {
+    if (e.key === 'Enter') {
+      updateValor(e, idcliente, nombre);
+      e.preventDefault();  // Prevenir el comportamiento por defecto del Enter.
+    }
+  };
+
+ 
   return ( 
-    zona.map(({_id, codt, razon_social, nombrezona, nombre, valorunitario, idcliente, isCheked}: any)=> {
+    zona.map(({_id, codt, razon_social, nombrezona, direccion, nombre, valorunitario, idcliente, isCheked}: any, index: any)=> {
     return (
       <TableRow
-        key={_id}
+        key={index}
       >
         <TableCell component="th" scope="row" align="center">
           <Checkbox
-            checked={isCheked ?isCheked :false}
-            onChange={()=>addValues(idcliente, valorunitario)}
+            checked={isCheked  ?isCheked :false}
+            onChange={(e)=>addValues(idcliente, valorunitario, _id, e)}
             inputProps={{ 'aria-label': 'controlled' }}
           />
         </TableCell>
-        <TableCell align="center" component="th">{nombrezona}</TableCell>
+        <TableCell align="center">{nombrezona}</TableCell>
+        <TableCell align="center">{direccion}</TableCell>
         <TableCell align="center">{codt}</TableCell>
         <TableCell align="center">{razon_social}</TableCell>
         <TableCell align="center">{nombre}</TableCell>
@@ -29,12 +58,13 @@ const RenderZonas = ({zona, updateValor, addValues}: any) => {
           <FormControl fullWidth sx={{ m: 1 }}>
           <InputLabel htmlFor={_id.toString()}>Valor Uni</InputLabel>
           <OutlinedInput
-            id={_id.toString()}
-            startAdornment={<InputAdornment position="start">$</InputAdornment>}
-            label="Amount"
-            defaultValue={valorunitario}
-            onKeyPress={(e)=>{updateValor(e, idcliente, nombre)}}
-          />
+              id={_id.toString()}
+              startAdornment={<InputAdornment position="start">$</InputAdornment>}
+              label="Amount"
+              value={valores[_id] || ''}
+              onChange={(e) => handleInputChange(e, _id)}
+              onKeyDown={(e) => handleKeyDown(e, idcliente, nombre)}
+            />
           </FormControl>
         </TableCell>
       </TableRow>
@@ -43,52 +73,63 @@ const RenderZonas = ({zona, updateValor, addValues}: any) => {
 }
 export default function RenderTable({zona}: any) {
   const [showSnack, setShowSnack] = useState(false);
+  const [isCheked, setIsCheked] = useState(false);
   const [message, setMessage] = useState("");
+  const [severity, setSeverity] = useState("");
   const [valorWithArray, setValorWithArray] = useState<{ _id: any; valorunitario: number; }[]>([]);
+  const router = useRouter();
+  const pathname = usePathname()
+  const searchParams = useSearchParams();
+  const page = searchParams.get('page');
+  const newValue = searchParams.get('newValue');
+  const search = searchParams.get('search');
+
 
   const [newValorUnitario, setNewValorUnitario] = useState({})
   const [newZona, setNewZona] = useState(zona)
-  const updateValor = async (event: any, idcliente: any, nombre: any) => {
-    if (event.key === 'Enter') {
-      event.preventDefault()
-      const {status} = await ChangeValorUnitario(Number(event.target.value), idcliente)
-      if (status) {
-        setShowSnack(true)
-        setMessage(`Valor Unitario Actualizado al cliente: ${nombre}`)
-      }
-    }
-  }
-  const addValues = (id: any, valorunitario: number) => {
-    const index = valorWithArray.some(({ _id }) => _id === id);
-    if (!index) {
-      setValorWithArray((state) => [...state, {_id: id, valorunitario}])
-    }else{
-      setValorWithArray(valorWithArray.filter(({_id})=> {return _id !== id})) 
-    }
-  } 
- 
+
   useEffect(() => {
     setNewZona(zona)
-  }, [zona])
+  }, [zona, newValue])
   useEffect(() => {
-    const {value, replace, type}: any = newValorUnitario
+    const {valor: value, replace, typeValue: type, allUsers}: any = newValorUnitario
+    const typeValue = replace==0 ?value :replace
+    const typeParams = replace==0 ?type :'replace'
  
     const data = valorWithArray.map(({_id, valorunitario}: any)=>{
-      const newValue = type==="porcentege" || type==undefined ? valorunitario +((valorunitario*Number(value))/100) : valorunitario+Number(value)
+      const newValue = type==="porcentaje" || type==undefined ? valorunitario +((valorunitario*Number(value))/100) : valorunitario+Number(value)
+      const valorUnitario = replace==0 ?Math.round(parseFloat(newValue)) :Number(replace)
       return {
         _id,
-        valorUnitario: replace ?Number(replace) :Math.round(parseFloat(newValue))
+        valorUnitario
       }
     })
- 
-    if( value || replace|| type ) saveData(data)
+   
+    if( value || replace|| type) saveData(data, allUsers, typeParams, typeValue)
   }, [newValorUnitario])
 
-  const saveData = async (data: any) => {
-    const {status} = await ChangeValorUnitarioAll(data)
+  const saveData = async (data: any, allUsers: boolean, type: string, replaceParams: any) => {
+    let status = false;
+    if(allUsers) {
+      const response =  await ChangeValorUnitarioAll(replaceParams, type)
+      status= response.status
+    }
+    if(!allUsers){
+      const response =  await ChangeValorUnitarioSelected(data)
+      status= response.status
+    }
+ 
     if (status) {
       setShowSnack(true)
       setMessage("Valor Unitario actualizado")
+      setSeverity('success')
+      setValorWithArray([])
+      setIsCheked(false)
+      router.push(`${pathname}?page=${page}&search=${search}&newValue=${replaceParams}`, undefined)
+    }
+    else if(!status){
+      setMessage("Houston tenemos un problema")
+      setSeverity('error')
     }
   }
 
@@ -97,23 +138,59 @@ export default function RenderTable({zona}: any) {
       return{
         ...e,
         _id: e.idcliente,
-        isCheked: event.target.checked  
+        isCheked: event.target.checked
       }
     })
-    if(event.target.checked) setValorWithArray(data)
-    if(!event.target.checked) setValorWithArray([])
+    
+    if(event.target.checked) {setValorWithArray(data), setIsCheked(true)}
+    if(!event.target.checked) {setValorWithArray([]), setIsCheked(false)}
     setNewZona(data)
   }
-
-  const validateIfIsSelectd = (e: any) => {
-    if(valorWithArray.length===0){
-      setShowSnack(true)
-      setMessage("Selecciona al menos una fila!!!")
-    }else{
-      setNewValorUnitario(e)
+  const updateValor = async (event: any, idcliente: any, nombre: any) => {
+    if (event.key === 'Enter') {
+      event.preventDefault()
+      const {status} = await ChangeValorUnitario(Number(event.target.value), idcliente)
+      if (status) {
+        setShowSnack(true)
+        setMessage(`Valor Unitario Actualizado al cliente: ${nombre}`)
+        setSeverity('success')
+        
+      }
     }
-      
   }
+  const addValues = (idCliente: any, valorunitario: number, id: any, event: any) => {
+ 
+    const index = valorWithArray.some(({ _id }) => _id === idCliente);
+    if (!index) {
+      setValorWithArray((state) => [...state, {_id: idCliente, valorunitario}])
+    }else{
+      setValorWithArray(valorWithArray.filter(({_id})=> {return _id !== idCliente})) 
+    }
+   
+    const updateZona = newZona.map((e: { _id: any; }) => {
+      if (e._id === id) {
+        return { ...e, isCheked: event.target.checked };
+      }
+      return e;
+    });
+  
+    setNewZona(updateZona);
+    
+  } 
+  const validateIfIsSelectd = (e: any) => {
+    if(e.allUsers){
+      setNewValorUnitario(e)
+    } else {
+      if(valorWithArray.length===0){
+        setShowSnack(true)
+        setMessage("Selecciona al menos una fila!!!")
+        setSeverity('error')
+      }else{
+        setNewValorUnitario(e)
+      }
+    }
+  }
+
   return(
     <TableContainer component={Paper}>
       <InputZones onSend={validateIfIsSelectd} />
@@ -122,11 +199,13 @@ export default function RenderTable({zona}: any) {
             <TableRow>
               <TableCell align="center">
                 <Checkbox
+                  checked={isCheked}
                   onChange={addValuesAll}
                   inputProps={{ 'aria-label': 'controlled' }}
                 />
               </TableCell>
               <TableCell align="center">Zona</TableCell>
+              <TableCell align="center">Dirección</TableCell>
               <TableCell align="center">Codt</TableCell>
               <TableCell align="center">Razon Social</TableCell>
               <TableCell align="center">Nombre</TableCell>
@@ -142,7 +221,7 @@ export default function RenderTable({zona}: any) {
         </TableBody>
       </Table>
       <PaginationTable total={zona[0]?.total ??0} />
-      <Snack show={showSnack} setShow={()=>setShowSnack(false)} message={message} />
+      <Snack show={showSnack} setShow={()=>setShowSnack(false)} message={message} severity={severity} />
     </TableContainer>
   )
 }
