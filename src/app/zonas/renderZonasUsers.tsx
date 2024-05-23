1a53ebd8f7ee42fcccd7cccda3bf3db22735a133
fernandooj <fernandooj@ymail.com>
2024-05-23 15:10:33 -0500

    fix render zonas

--- src/app/zonas/renderZonasUsers.tsx
+++ src/app/zonas/renderZonasUsers.tsx
@@ -1,7 +1,8 @@
 import {fetchZonasByUser} from '../store/fetch-zona' 
 import RenderTable from './table'
 
-export const RenderZonasUsers = async function RenderZonasUsers({limit, page, idZone, type, search}: any) {
-  let zona = await fetchZonasByUser(limit, page, idZone, type, search);
+export const RenderZonasUsers = async function RenderZonasUsers({limit, page, idZone, type, search, newValue}: any) {
+  const zona = await fetchZonasByUser(limit, page, idZone, type, search, newValue);
+
   return <RenderTable zona={zona} />;
 }
